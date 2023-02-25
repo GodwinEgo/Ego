b46b7a9dbfffd1fc642716a5458b1fa89a2a0769
GodwinEgo <godwinehile04@gmail.com>
2023-02-25 14:35:06 +0100

    Ego

--- src/components/NavbarElements.jsx
+++ src/components/NavbarElements.jsx
@@ -41,7 +41,12 @@ color: #808080;
 export const NavMenu = styled.div`
 display: flex;
 align-items: center;
-width: 100%;
+margin-right: -24px;
+/* Second Nav */
+/* margin-right: 24px; */
+/* Third Nav */
+/* width: 100vw;
+white-space: nowrap; */
 @media screen and (max-width: 768px) {
 	display: none;
 }
