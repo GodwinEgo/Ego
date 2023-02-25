5c409704b05f8ed67104d7d4613e46fc408371a7
GodwinEgo <godwinehile04@gmail.com>
2023-02-25 09:29:25 +0100

    Signin

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import { Avatar, CssBaseline, Button, Link, FormControlLabel, Grid, TextField, Typography, Container } from '@mui/material'
-import { Box } from '@mui/material';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { CheckBox, LockOutlined } from '@mui/icons-material'
 import Navbar from '../components/Navbar';
 
-
-
-function Copyright ( props ) 
+function Copyright ( props )
 {
   return (
-    <Typography variant="body2" color="text.secondary" align='center' { ...props }>
-      { 'Copyright @' }
-      <Link color='inherit' href="#" >
-        Ego
+    <Typography variant="body2" color="text.secondary" align="center" { ...props }>
+      { 'Copyright © ' }
+      <Link color="inherit" href="#">
+        Your Website
       </Link>{ ' ' }
       { new Date().getFullYear() }
-      { ' . ' }
+      { '.' }
     </Typography>
   );
 }
 
 const theme = createTheme();
 
-
-const SignIn = () =>
+export default function SignIn ()
 {
   const handleSubmit = ( event ) =>
   {
@@ -50,42 +56,41 @@ const SignIn = () =>
           } }
         >
           <Avatar sx={ { m: 1, bgcolor: 'secondary.main' } }>
-            <LockOutlined />
+            <LockOutlinedIcon />
           </Avatar>
-          <Typography component="h1" variant='h5'>
-            Sign In
+          <Typography component="h1" variant="h5">
+            Sign in
           </Typography>
-          <Box component='form' onSubmit={ handleSubmit } noValidate sx={ { mt: 1 } }>
+          <Box component="form" onSubmit={ handleSubmit } noValidate sx={ { mt: 1 } }>
             <TextField
-              margin='normal'
+              margin="normal"
               required
               fullWidth
-              id='email'
-              label='Email Address'
-              name='email'
-              autoComplete='email'
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
               autoFocus
             />
             <TextField
-              margin='normal'
+              margin="normal"
               required
               fullWidth
-              id='password'
-              type='password'
-              label='Password '
-              name='password'
-              autoComplete='current-password'
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
             />
-
             <FormControlLabel
-              control={ <CheckBox value="remember" color="primary" /> }
-              label="Remember Me"
+              control={ <Checkbox value="remember" color="primary" /> }
+              label="Remember me"
             />
             <Button
-              type='submit'
+              type="submit"
               fullWidth
               variant="contained"
-              sx={ { mat: 3, mb: 2 } }
+              sx={ { mt: 3, mb: 2 } }
             >
               Sign In
             </Button>
@@ -108,10 +113,3 @@ const SignIn = () =>
     </ThemeProvider>
   );
 }
-export default SignIn
-
-
-
-
-
-
